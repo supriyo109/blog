1ca583ac8ea1174587cc77eaafc3013eb046b319
supriyo109 <supriyogantait20210@gmail.com>
2025-08-23 18:26:42 +0530

    fix: corrected mongo connection

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 
 const connectDb = async() => {{
     try{
-        await mongoose.connect(process.env.MONGO_URL)
-        console.log("MongoDB connection successful");
+await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });        console.log("MongoDB connection successful");
     }catch(error){
         console.log("Mongodb Connection Error", error);
     }
